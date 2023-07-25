28b03fa993ddbf230d2700b2a4b3e6b7517742c6
NitinThakur369 <139260986+NitinThakur369@users.noreply.github.com>
2023-07-25 12:39:36 +0530

    index(HooksCounter).jsx

---
+++ index(HooksCounter).jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Counter from './HooksCounter';
+import "../src/Counter.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+
+    <Counter/>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
